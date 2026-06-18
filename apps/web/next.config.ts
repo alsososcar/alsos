f2527507899a75a5e91dd3ed966a8905f1ca031a
43alsosc <43alsosc@stud.akademiet.no>
2026-06-18 15:52:48 +0200

    feat(web) added portless for https dev environent

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from "next"
 
 const nextConfig: NextConfig = {
   transpilePackages: ["@alsos/ui"],
+  allowedDevOrigins: ["alsos.localhost", "*.alsos.localhost"],
+  reactCompiler: true,
+  logging: {
+    browserToTerminal: true,
+  },
 }
 
 export default nextConfig
