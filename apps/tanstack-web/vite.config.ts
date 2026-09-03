ffd4024917cec85ae0c14b0e6092627b913ca117
Alsos <114300964+alsososcar@users.noreply.github.com>
2026-09-03 21:39:20 +0200

    chore: remove unused plugin

--- apps/tanstack-web/vite.config.ts
+++ apps/tanstack-web/vite.config.ts
@@ -1,5 +1,4 @@
 import { cloudflare } from "@cloudflare/vite-plugin";
-import contentCollections from "@content-collections/vite";
 import tailwindcss from "@tailwindcss/vite";
 import { devtools } from "@tanstack/devtools-vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
